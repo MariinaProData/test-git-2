3cbb6ad96f62268886c75d75e6482c35684407ae
Marina <m.prokopeva80@gmail.com>
2023-07-17 11:46:52 +0300

    feature/user-access

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log('Hello Word!')
-console.log('Hello Word!')
+
+const age = 20;
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы НЕ совершеннолетний')
+}
